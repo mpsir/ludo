c3fb39c4d3124b9bccb3b2416d4d64f58a0600d0
mpsir <super1mpsir@gmail.com>
2020-11-09 22:11:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,23 +8,27 @@ var filesToCache = [
   './js/main.js'
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       console.log(e.request)
       if (response) { return response; }
-      
-      var r_url = e.request.url; 
-      if (r_url == '/' || r_url == './index.html' || r_url == 'index.html') {
+
+      var r_url = e.request.url;
+      if (
+        r_url == '/' ||
+        r_url == './index.html' ||
+        r_url == 'index.html'
+      ) {
         console.log(888)
       }
 
