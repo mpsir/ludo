c450efd24181031c7f99ab7f5b2b5194f274c191
mpsir <super1mpsir@gmail.com>
2020-11-10 17:53:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -33,7 +33,55 @@ self.addEventListener('fetch', function (e) {
         r_url == this_url + '/index.html' ||
         r_url == this_url + '/' 
       ) {
-        return new Response("<h1>Hello!</h1>", {
+        return new Response(
+          `
+          <!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Ludo</title>
+  <link rel="manifest" href="manifest.json">
+  <link rel="icon" href="favicon.ico" type="image/x-icon" />
+  <link rel="apple-touch-icon" href="images/hello-icon-152.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="theme-color" content="white" />
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="PC">
+  <meta name="msapplication-TileImage" content="images/hello-icon-144.png">
+  <meta name="msapplication-TileColor" content="#FFFFFF">
+</head>
+
+<body>
+
+  <h3 style="text-align: center;">
+    Yes
+  </h3>
+
+  
+
+  <script>
+
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('./sw.js')
+          .then(function (registration) {
+            console.log('sw sussess reg')
+            //location.reload(); 
+          }, function (err) {
+            console.log('ServiceWorker registration failed: ', err);
+          });
+      });
+    }
+
+  </script>
+
+</body>
+
+</html>
+          `
+        , {
           headers: {'Content-Type': 'text/html'}
         })
       }
